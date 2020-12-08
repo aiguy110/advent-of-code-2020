ed275c0897f775455cd84684cc2e4902121b80a6
Josiah Hunsinger <fencepost13302@gmail.com>
2020-12-07 23:34:58 -0500

    Finished Day 4

--- day_04/solution2.py
+++ day_04/solution2.py
@@ -1,4 +1,5 @@
 import sys
+import re
 
 def load_passport_data(file_name):
     passports = []
@@ -14,12 +15,29 @@ def load_passport_data(file_name):
                 k, v = kv_pair.split(':')
                 current_passport[k] = v
             
+        if current_passport != {}:
+            passports.append( current_passport )
+
     return passports
 
 def is_valid(passport):
-    required_fields = ['ecl', 'pid', 'eyr', 'hcl', 'byr', 'iyr', 'hgt']
-    for req_field in required_fields:
+    required_fields = [
+        ( 'byr', r'^\d{4}$', lambda x: (int(x) >= 1920) and (int(x) <= 2002) ),
+        ( 'pid', r'^\d{9}$', lambda x: True ),
+        ( 'eyr', r'^\d{4}$', lambda x: (int(x) >= 2020) and (int(x) <= 2030) ),
+        ( 'hcl', r'^#[0-9a-f]{6}$', lambda x: True ),
+        ( 'ecl', r'^(amb|blu|brn|gry|grn|hzl|oth)$', lambda x: True ),  
+        ( 'iyr', r'^\d{4}$', lambda x: (int(x) >= 2010) and (int(x) <= 2020) ),
+        ( 'hgt', r'^\d+(cm|in)$', lambda x: (int(x[:-2]) >= 150) and (int(x[:-2]) <= 193) if 'cm' in x else (int(x[:-2]) >= 59) and (int(x[:-2]) <= 76) )
+    ]
+    for req_field, field_regex, validation_fn in required_fields:
         if req_field not in passport:
+            return False
+        
+        if not re.match(field_regex, passport[req_field]):
+            return False
+        
+        if not validation_fn( passport[req_field] ):
             return False
     
     return True
